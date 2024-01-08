01a72b52ef15816ea139d242db83aa07892a2136
petro-tech <peteronialaexists@gmail.com>
2024-01-08 03:37:14 -0800

    Changes

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+
+# A class Mylist that inherits from list
+
+class MyList(list):
+    def print_sorted(self):
+
+        
+        # Printing sorted list
+
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+Python - Inheritance
+
+Task 0 - Lookup
+ a function that returns the list of available attributes and methods of an object
